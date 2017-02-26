5a4b7b25cfa34a0db36ac05725dc0a7e57c9ea2b
Gittel Volpo <gvolop@work.capital>
2017-02-26 14:32:52 +0200

    update deps

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,10 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :big_query,
+  bigquery_private_key_path: "./work-capital-data-analysis-cc2127a7a372.json"
+
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
--- test/big_query_test.exs
+++ test/big_query_test.exs
@@ -2,7 +2,13 @@ defmodule BigQueryTest do
   use ExUnit.Case
   doctest BigQuery
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "big query" do
+    project_id = "work-capital-data-analysis"
+    query_string = "SELECT * FROM [work-capital-data-analysis:ProductionViews.OMIE_DECISION] WHERE uuid = 'a9382faffc2c0b24b028c47f4c3b17b5'"
+    query =  %BigQuery.Types.Query{query: query_string, timeoutMs: 1000*120}
+    {:ok, %BigQuery.Types.QueryResultsResponse{rows: [res | _]}} = BigQuery.Job.query(project_id, query)
+    %BigQuery.Types.QueryResultRow{f: [_, %BigQuery.Types.QueryResultCell{v: uuid} | _]} = res
+
+    assert uuid == "a9382faffc2c0b24b028c47f4c3b17b5"
   end
 end
